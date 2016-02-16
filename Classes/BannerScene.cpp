f992c6612c0e676a20779225cc04ba9551b4d386
fusijie <fusijie@vip.qq.com>
2016-02-16 14:45:30 +0800

    add cocos logo.

--- Classes/BannerScene.cpp
+++ Classes/BannerScene.cpp
@@ -50,6 +50,15 @@ bool Banner::init()
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
     
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("HelloWorld.png");
+    
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+    
     
     //展示广告
     const char* placementID = "";
